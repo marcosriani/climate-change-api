110f1db56877b0f2faf4312cd98722a309e17e50
MarcosCabrini Riani_dos_Reis <marcosriani@hotmail.com>
2023-09-05 10:03:07 +0100

    First part of a scraping websites API

---
+++ index.js
@@ -0,0 +1,34 @@
+const PORT = 8000;
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = express();
+
+const articles = [];
+
+app.get('/', (req, res) => {
+  res.json('Welcome to my Climate Change News API');
+});
+
+app.get('/news', (req, res) => {
+  axios
+    .get('https://www.theguardian.com/environment/climate-crisis')
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr('href');
+        articles.push({ title: title, url: url });
+      });
+
+      res.json(articles);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
